41d2dc06c85b7b7d8c3a5b6ddf98cc34542633b9
Cerdic <cedric@yterium.com>
2021-07-20 13:52:15 +0200

    lancement des tests plus robuste

--- tests/simpletest/lanceur_spip.php
+++ tests/simpletest/lanceur_spip.php
@@ -3,10 +3,17 @@
  * Démarre SPIP afin d'obtenir ses fonctions depuis
  * les jeux de tests unitaires de type simpletest
  */
-$remonte = '../';
-while (!is_dir($remonte . 'ecrire')) {
-	$remonte = "../$remonte";
+$remonte = "";
+while (!is_file($remonte."test.inc") and !is_dir($remonte.'ecrire/'))
+	$remonte = $remonte."../";
+foreach ([$remonte."test.inc", $remonte."tests/test.inc", $remonte."tests/tests/legacy/test.inc"] as $f) {
+	if (is_file($f)){
+		require $f;
+		break;
+	}
 }
-require $remonte . 'tests/test.inc';
+if (!defined('_SPIP_TEST_INC')) {
+	die('Impossible de trouver test.inc depuis ' .getcwd());
+}
 
 demarrer_simpletest();
